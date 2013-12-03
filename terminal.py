adac3c5dd00c7a37a693bee0ba2a11dec0cf9851
tacio <tcgtavares@gmail.com>
2013-12-03 19:33:13 -0200

    registering files on load

--- terminal.py
+++ terminal.py
@@ -1,11 +1,18 @@
 #coding: utf-8
 
-from client_download import download_file_view, list_files_view
+import os
+
+from client_download import download_file_view, list_files_view, register_as_owner
 
 print "\n\n"
 print "--- Torrent-ITA ---"
 print "Comandos disponíveis: LIST_FILES e DOWNLOAD <FILENAME>"
 print "\n\n"
+
+# register_all_files_availables
+for f in os.listdir('./files'):
+	register_as_owner(f)
+
 
 while True:
 	command = raw_input()
