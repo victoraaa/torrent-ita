0f3cefa7b74e6916d7eea1e49d327803f9e0276b
Lucas Duailibe <lucasds@gmail.com>
2013-12-04 07:52:12 -0200

    final

--- tracker.py
+++ tracker.py
@@ -36,10 +36,7 @@ class File(object):
 
 class Tracker(object):
     def __init__(self, port=34000):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind(('', port))
-
+        self.port = port
         self.commands = {
             'REGISTER_AS_OWNER': self.register,
             'LIST_FILES': self.list_files,
@@ -47,68 +44,48 @@ class Tracker(object):
         }
         self.files = dict()
 
-        t = Thread(target=self.check_providers)
-        t.start()
-
     def loop(self):
-        self.socket.listen(5)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', self.port))
+        s.listen(5)
         print '* Listening'
         while True:
             try:
-                conn, addr = self.socket.accept()
+                conn, addr = s.accept()
                 print '* New connection: {}'.format(addr[0])
+                # Nova conexão -> cria uma nova thread para ela, que roda self.client
                 thread.start_new_thread(self.client, (conn, addr))
             except KeyboardInterrupt:
                 print '* Exiting'
-                self.socket.close()
-
-    def check_providers(self):
-        while False:
-            providers = []
-            for f in self.files:
-                providers.extend(f.providers.values())
-            providers = set(providers)
-
-            for provider in providers:
-                thread.start_new_thread(self.send_ping, (provider))
-
-            time.sleep(2*60)
-
-    def send_ping(self, addr):
-        conn = socket.create_connect(addr)
-        conn.sendall(json.dumps({'method': 'PING', 'type': 'REQUEST'}))
-        print '-> PING: {}'.format(addr[0])
-        response = conn.recv(1024)
-        if response:
-            response = json.loads(response)
-            if response['method'] == 'PING' and response['type'] == 'RESPONSE':
-                return
-        for f in self.files:
-            f.remove_provider(addr)
+                s.close()
 
     def client(self, conn, addr):
+        # Thread que lida com as requisições de clientes
         data = conn.recv(1024)
         if data:
             print '<- {}: {}'.format(addr[0], data)
             data = json.loads(data)
             func = self.commands[data['method']]
-            response = func(data) or dict()
+            response = func(data) or dict()  # Resposta computada a ser enviada ao cliente
             response.update({'method': data['method'], 'type': 'RESPONSE'})
-            print response
             print '-> {}: {}'.format(addr[0], json.dumps(response))
             conn.sendall(json.dumps(response))
             conn.close()
 
     def register(self, data):
+        # Registrar um cliente como provider de uma parte de um arquivo
         name = data['file']
         if name not in self.files:
             self.files[name] = File(name, data['MD5'])
         self.files[name].register_provider(data['part_number'], (data['IP'], data['port_number']))
 
     def list_files(self, data):
+        # Recuperar os arquivos que já foram registrados
         return dict(files=self.files.keys())
 
     def get_tracker(self, data):
+        # Recuperar a lista de providers de um arquivo
         if not data['file'] in self.files.keys():
             return {'error': 'FILE_NOT_AVAILABLE'}
         return self.files[data['file']].get_json()
